16afc23cae00073874b1dbef14383b45cda9d715
shouvikmajumder <shouvikprism@gmail.com>
2024-11-23 16:02:29 -0500

    two-sum on day 2

--- leetcode.py
+++ leetcode.py
@@ -1,14 +1,15 @@
 # contains duplicate 
 # Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+# Two- sum
 
-def hasDuplicate(self, nums: List[int]) -> bool:
-    check = []
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    check = {}
+
+    for index,value in enumerate(nums):
+        diff = target - value
+        if diff in check:
+            return [check[diff],index]
+        check[value] = index
     
-    for i in nums: 
-        if i not in check:
-            check.append(i)
-        elif i in check: 
-            return True 
-    return False
 
     #This is O(n) time complexity
